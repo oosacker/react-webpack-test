e55528cf1ef8693f7f095f35df8de522db2685f3
Natsuki Hasegawa <oosacker@gmail.com>
2020-07-20 21:13:39 +1200

    init

---
+++ src/js/components/Form.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: "-"
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState(() => {
+      return {
+        value
+      };
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <h3>{this.state.value}</h3>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+}
+
+export default Form;
+
+const wrapper = document.getElementById("container");
+wrapper ? ReactDOM.render(<Form />, wrapper) : false;
